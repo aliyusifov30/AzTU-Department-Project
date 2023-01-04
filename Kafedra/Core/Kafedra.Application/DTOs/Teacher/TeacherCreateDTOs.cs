88c9b4fb756d5f993e0eb85067af8d0ca8147522
MzadeTural <tu201906140@code.edu.az>
2023-01-04 19:32:53 +0400

    Fix some bugs

---
+++ Kafedra/Core/Kafedra.Application/DTOs/Teacher/TeacherCreateDTOs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kafedra.Application.DTOs.Teacher
+{
+    internal class TeacherCreateDTOs
+    {
+    }
+}
---
+++ Kafedra/Presentation/Kafedra.MVC/Areas/Azadedu/Controllers/TeacherController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Kafedra.MVC.Areas.Azadedu.Controllers
+{
+    public class TeacherController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
